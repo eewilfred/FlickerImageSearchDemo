26078df7422b3aab78a59a26266fbc1ab7e26785
Edwin Wilson <ewilson@altayer.com>
2021-10-11 12:01:28 +0400

    Update collection view cell to show images from image download manager

--- FlickerImageSearch/FlickerImageSearch/Scenes/image search/Views/ImageSearchCollectionViewCell/ImageSearchCollectionViewCell.swift
+++ FlickerImageSearch/FlickerImageSearch/Scenes/image search/Views/ImageSearchCollectionViewCell/ImageSearchCollectionViewCell.swift
@@ -17,10 +17,48 @@ class ImageSearchCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet private weak var imageView: UIImageView!
 
+    private var imageDownLoadTask: URLSessionDataTask?
+
     override func awakeFromNib() {
 
         super.awakeFromNib()
         layer.cornerRadius = 5.0
+        addObserver()
+    }
+
+    deinit {
+
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    override func prepareForReuse() {
+
+        super.prepareForReuse()
+        imageView.image = nil
+        if let urlString = presentation?.imageURLString,
+           let url = URL(string: urlString) {
+            ImageDownloadManager.shared.stopImageDownloadTask(url: url)
+        }
+    }
+
+    private func addObserver() {
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleImageUpdates(notification:)),
+            name: .imageDownloadCompleted,
+            object: nil
+        )
+    }
+
+    @objc private func handleImageUpdates(notification: Notification) {
+
+        if let imageUrl = presentation?.imageURLString,
+           let image = notification.userInfo?[imageUrl] as? UIImage {
+            DispatchQueue.main.async { [weak self] in
+                self?.imageView.image = image
+            }
+        }
     }
 
     private func updateUI() {
@@ -30,10 +68,8 @@ class ImageSearchCollectionViewCell: UICollectionViewCell {
         }
 
         if let url = URL(string: presentation.imageURLString) {
-            do {
-                imageView.image = UIImage(data: try Data(contentsOf: url), scale: 1)
-            } catch let error {
-                print(error.localizedDescription)
+            if let image = ImageDownloadManager.shared.getImageForURL(url: url) {
+                imageView.image = image
             }
         }
     }
